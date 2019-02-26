0d93404831cf2c1cecc840b1723886c256a2c6b9
panstone <panaskin.dim@yandex.ru>
2019-02-27 00:10:18 +0300

    devise add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :models, path: 'auth', path_names: { sign_in: 'login', 
+												sign_out: 'logout', 
+												password: 'secret', 
+												confirmation: 'verification', 
+												unlock: 'unblock',
+												registration: 'register',
+												sign_up: 'registration' },
+												controllers: {
+        														registrations: 'models/registrations'
+     															 }
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'hellos#index'
 end
